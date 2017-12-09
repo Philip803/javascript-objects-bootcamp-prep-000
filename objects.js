498ead91954cae64c41f4f3303b2914021d9aef2
philip ka sing leung <philip.leung90@gmail.com>
2017-12-09 19:44:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,7 +8,7 @@ function updatePlaylist(obj , name , song){
   return obj;
 }
 
-function removeFromPlaylist(){
-  
-  
+function removeFromPlaylist(obj , name){
+  delete obj[name];
+  return obj;
 }
